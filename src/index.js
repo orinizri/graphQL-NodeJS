dbcdb62df90856ace54d48d447888ae693e3133a
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-06-16 21:41:14 +0300

    single product

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import { createServer } from '@graphql-yoga/node'
 
 // type definitions
-
+const schema = {
+    typeDefs : `
+    type Query {
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: boolean!
+    }`
+}
 // resolver
+const resolvers = {
+    Query: {
+        title() {
+            return "The War of Art"
+        },
+        price() {
+            return 17.5
+        },
+        releaseYear() {
+            return null
+        },
+        rating() {
+            return 5
+        },
+        inStock() {
+            return true
+        }
+    }
+}
+const server = createServer({schema, resolvers});
 
-
-
-const server = createServer({})
-server.start()
+server.start();
