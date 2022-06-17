b4fedbf047f7e0fce3bbc6141e10700327ec3128
Ori Nizri Tal <ori.nizri.tal@gmail.com>
2022-06-17 17:31:46 +0300

    users and posts schema

--- src/index.js
+++ src/index.js
@@ -1,36 +1,66 @@
 import { createServer } from '@graphql-yoga/node'
 
-// type definitions
-const schema = {
-    typeDefs : `
-    type Query {
-        title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        inStock: boolean!
-    }`
+const users = [
+    {
+        id: '1',
+        name: "Andrew",
+        email: "andrew@example.com"
+    },
+    {
+        id: '2',
+        name: "Sarah",
+        email: "sarah@sarah.com"
+    }
+]
+
+const typeDefs =
+    `
+type Query {
+    me: User!
+    post: Post!
+    users(query: String): [User!]!
 }
-// resolver
-const resolvers = {
-    Query: {
-        title() {
-            return "The War of Art"
-        },
-        price() {
-            return 17.5
-        },
-        releaseYear() {
-            return null
-        },
-        rating() {
-            return 5
-        },
-        inStock() {
-            return true
+type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int!
+}
+type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+}`;
+const query = {
+    users(p, args, ctx, info) {
+        if (!args.query) return users;
+        return users.filter((user) => user.name.toLowerCase().includes(args.query.toLowerCase()))
+    },
+    me() {
+        return {
+            id: "123a",
+            name: "mike",
+            email: "mike@example",
+            age: 32
+        }
+    },
+    post() {
+        return {
+            id: '123',
+            title: 'robin hood',
+            body: 'bla bla bla',
+            published: true
         }
     }
-}
-const server = createServer({schema, resolvers});
+};
+const server = createServer({
+    schema: {
+        typeDefs,
+        resolvers: {
+            Query: query
+        }
+    }
+});
 
 server.start();
